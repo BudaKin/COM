6457823419ef5668b8c50cedaf4a2566f8c90e30
BudaKin <igorcrashtom@hotmail.com>
2025-09-23 10:38:18 -0300

    Aula 23/09/25

--- COM2/block-codes.py
+++ COM2/block-codes.py
@@ -3,6 +3,9 @@ import komm
 import numpy as np
 import matplotlib.pyplot as plt
 
+rng = np.random.default_rng(seed=42)
+komm.global_rng.set(rng)
+
 st.title("Códigos de bloco")
 
 filename = "mario.txt"
@@ -13,7 +16,7 @@ with open(filename) as f:
         line = [int(x) for x in line.strip()]
         image.append(line)
     image = np.array(image)
-    image = np.kron(image, np.ones((7,7), dtype=int))
+    image = np.kron(image, np.ones((4,4), dtype=int))
     height, width = image.shape
 
     def plot_bits(bits):
@@ -26,17 +29,36 @@ with open(filename) as f:
     p = st.slider(label="Probabilidade de troca do BSC ($p$)", min_value=0.0, max_value=0.5, value=0.08)
 
     bsc = komm.BinarySymmetricChannel(p)
+    # code = komm.HammingCode(3)
+    code = komm.ReedMullerCode(1, 4)
+    decoder = komm.SyndromeTableDecoder(code)
 
-    cols = st.columns(2)
+    st.write((code.length, code.dimension))
+    st.write(f"**Minimum distance:** {code.minimum_distance()}")
+    st.write(f"**Code rate:** {code.rate:.2%}")
+
+    cols = st.columns(3)
 
     u = image.reshape(height*width)
 
+    def get_ber(bits, bits_hat):
+        return np.mean(bits != bits_hat)
+
     with cols[0]:
         plot_bits(u)
-
-    # Não codificado
-    b_nc = bsc.transmit(u)
+    
     with cols[1]:
+        # Não codificado
+        b_nc = bsc.transmit(u)
         plot_bits(b_nc)
+        st.write(f"{get_ber(u, b_nc):.2%}")
+
+    with cols[2]:
+        # codificado
+        v = code.encode(u)
+        b_c = bsc.transmit(v)
+        u_hat = decoder.decode(b_c)
+        plot_bits(u_hat)
+        st.write(f"{get_ber(u, u_hat):.2%}")
 
     
