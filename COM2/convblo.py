69f76291340509f11ade9bcd498cb29213bc9a7b
BudaKin <igorcrashtom@hotmail.com>
2025-10-14 11:14:47 -0300

    Aula 14/10/25

--- COM2/convblo.py
+++ COM2/convblo.py
@@ -1,13 +1,33 @@
 import komm
-import numpy as np
 
-# komm.ConvolutionalCode([[0b11, 0b10, 0b11],[0b10, 0b01, 0b01]])
+convcode = komm.ConvolutionalCode([0b111, 0b101])
 
-code = komm.ConvolutionalCode([[0b111, 0b101]])
+blockcode = komm.TerminatedConvolutionalCode(convolutional_code=convcode,num_blocks=5,mode="direct-truncation") # Transformação conv -> block (Terminação truncamento direto)
 
-u = np.array([1,0,1,1,0,0,0])
+nk = (blockcode.length, blockcode.dimension)
 
-v = code.encode(u)
+blockcode.generator_matrix
 
-print(u)
-print(v)
+blockcode.minimum_distance()
+
+blockcode.encode([1,0,1,1,0]) # 11 10 00 01 01
+
+blockcode = komm.TerminatedConvolutionalCode(convolutional_code=convcode,num_blocks=5,mode="zero-termination") # Transformação conv -> block (Terminação estado 0)
+
+nk = (blockcode.length, blockcode.dimension)
+
+blockcode.generator_matrix
+
+blockcode.minimum_distance()
+
+blockcode.encode([1,0,1,1,0]) # 11 10 00 01 01 11 00
+
+blockcode = komm.TerminatedConvolutionalCode(convolutional_code=convcode,num_blocks=5,mode="tail-biting") # Transformação conv -> block ("Mordida de cauda")
+# Tail biting começa e termina no mesmo estado.
+nk = (blockcode.length, blockcode.dimension)
+
+blockcode.generator_matrix
+
+blockcode.minimum_distance()
+
+blockcode.encode([1,0,1,1,0]) # 00 10 00 01 01
